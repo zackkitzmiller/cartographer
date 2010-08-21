94b1275af5bbc80568e831b3c6e4514d14f69074
zackkitzmiller <zackkitzmiller@gmail.com>
2010-08-21 17:56:22 -0500

    bug fix

--- views/sitemap.php
+++ views/sitemap.php
@@ -6,7 +6,7 @@
 			    
     <?php foreach($pages as $page): ?>
 	<url>
-		<loc><?php echo site_url() . '/' . $page->url_title; ?></loc>
+		<loc><?php echo site_url('page/' . $page->url_title); ?></loc>
 		<lastmod><?php echo date('Y-m-d', $page->last_modified); ?></lastmod>
 		<changefreq>daily</changefreq>
 		<priority><?php echo ($page->include_in_page_list == 'y') ? '0.8' : '0.5'; ?></priority>
